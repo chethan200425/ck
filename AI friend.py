1ec2004b0c23270dee273183614c6dbe2c3cb084
unknown <chethankumarv69@gmail.com>
2022-12-13 18:21:35 -0800

    git commit	new file:   AI friend.py

---
+++ AI friend.py
@@ -0,0 +1,34 @@
+command=""
+while command !="friend go back":
+    command=input("<<")
+    if command=="hi friend":
+          print("Hi sir how can i help you....")
+    elif command=="friend open some cyber security related qustion and answers":
+          print("""sir i have some cyber security related qustion and answers,
+you loaded in my program..""")
+    elif command == "friend what is cyber security":
+            print("""Cyber security is the practice of defending computers, servers, mobile devices, electronic systems, networks, and data from malicious attacks.
+It's also known as information technology security or electronic information security.""")
+    elif command == 'help':
+        print(""" sir you are loaded qustions in my program are....
+1. friend what is cyber security?
+2.friend three pillor of cyber security?
+3.friend define AAA in cyber security?""")
+        break
+    else:
+           print("Thankyou sir have a nice day....")
+
+ 
+
+        
+
+          
+              
+
+
+  
+        
+  
+    
+
+    
